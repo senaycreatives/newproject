26e8de0cc831793b9ed418a3898610a9d30a1f7f
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2024-01-02 17:19:59 +0300

    doen

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import SignIn from './signIn';
-import './App.css';
-import Login from './Login';
-import AdminHome from './AdminHome';
-import { DataTable } from './DataTable';
-import LoginForm from './LoginForm';
-import { Details } from './Details';
-
+import SignIn from "./signIn";
+import "./App.css";
+import Login from "./Login";
+import AdminHome from "./AdminHome";
+import { DataTable } from "./DataTable";
+import LoginForm from "./LoginForm";
+import { Details } from "./Details";
+import { signIn } from "./signIn";
+import { AddData } from "./AddData";
+import { Layout } from "./Layout";
 const data = [
-  { id: 1, name: 'John Doe', email: 'john@example.com' },
-  { id: 2, name: 'Jane Smith', email: 'jane@example.com' },
+  { id: 1, name: "John Doe", email: "john@example.com" },
+  { id: 2, name: "Jane Smith", email: "jane@example.com" },
   // Add more data objects as needed
 ];
 
 function App() {
   return (
-<DataTable/>
-    );
+    //<Details/>
+    <DataTable />
+    // <Details />
+  );
 }
 
 export default App;
