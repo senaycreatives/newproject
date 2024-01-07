5f827f7473ec6887fc2228ae7629e492cfa90f03
haileamlak <amlakhaile@gmail.com>
2024-01-07 13:49:08 +0300

    some bug fix

--- src/UsersList.js
+++ src/UsersList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const users = [
   { id: 1, name: 'abebe', Role: 'Admin' },
@@ -10,7 +11,7 @@ const users = [
 const UserList = () => {
   return (
     <div className="px-20 py-10">
-      <h1 className="text-2xl font-bold my-4 text-center">User List</h1>
+      <h1 className="text-2xl font-bold my-4 text-center">Users List</h1>
       <table className="w-full">
         <thead>
           <tr>
@@ -29,6 +30,11 @@ const UserList = () => {
           ))}
         </tbody>
       </table>
+      <div className="text-center mt-4">
+      <Link to="/createUser" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Add New User
+        </Link>
+    </div>
     </div>
   );
 };
